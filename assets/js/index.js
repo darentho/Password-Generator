0e1c5e313861535b66a8f5f8ddc2d4d94d059870
darentho <106845099+darentho@users.noreply.github.com>
2022-07-07 17:22:53 -0500

    did some final debugging and fixing because code wouldn't work-it's all done now!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,82 +1,96 @@
-
 function generatePassword() {
-    //  I have defined the variable sets. I created an array for each var that contains separate characters that will then be appended to a blank array with concat.
+   //  I have defined the variable sets. I created an array for each var that contains separate characters that will then be appended to a blank array with concat.
     var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     var specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
-   //this is the empty array that will then be appended using concat.
+    //this is the empty array that will then be appended using concat.
     var selectedArray = [];
-
+  
     var passwordLength = getPasswordLength();
-
-
-    // These if statements determine the user options and then append them to the blank array I created.
-if (lowerCase) {
-    selectedArray = selectedArray.concat(lowerCaseArr);
-  }
-  if (upperCase) {
-    selectedArray = selectedArray.concat(upperCaseArr);
-  }
-  if (numericCharacters) {
-    selectedArray = selectedArray.concat(numArr);
-  }
-  if (specialCharacters) {
-    selectedArray = selectedArray.concat(specialArr);
+  
+    var typeSelected = false;
+    // This loop ensures the user selects at least one character type
+    while (typeSelected == false) {
+      var lowerCase = getChoice("lowercase");
+      var upperCase = getChoice("uppercase");
+      var numericCharacters = getChoice("numeric");
+      var specialCharacters = getChoice("special");
+      if ((lowerCase) || (upperCase) || (numericCharacters) || (specialCharacters)) {
+        typeSelected = true;
+      } else {
+        window.alert("You must select at least one character type.")
+      }
+    }
+  
+     // These if statements determine the user options and then append them to the blank array.
+     //lowerCase, upperCase etc are the variables used for the "While" Loop with the getChoice function.
+    if (lowerCase) {
+      selectedArray = selectedArray.concat(lowerCaseArr);
+    }
+    if (upperCase) {
+      selectedArray = selectedArray.concat(upperCaseArr);
+    }
+    if (numericCharacters) {
+      selectedArray = selectedArray.concat(numArr);
+    }
+    if (specialCharacters) {
+      selectedArray = selectedArray.concat(specialArr);
+    }
+  
+    var passwordString = "";
+    // This loop will take the appended array, randomly choose from them, then append the selections to generate a password.
+    for (var i = 0; i < passwordLength; i++) {
+      passwordString += selectedArray[Math.floor(Math.random() * (selectedArray.length))];
+    }
+  
+    return passwordString;
   }
   
-
-  var passwordString = "";
-// This loop will take the appended array, randomly choose from them, then append the selections to generate a password.
-for (var i = 0; i < passwordLength; i++) {
-  passwordString += selectedArray[Math.floor(Math.random() * (selectedArray.length))];
-}
-return passwordString;
-}
-
-function getPasswordLength() {
+  function getPasswordLength() {
     var userChoice = 0;
     while ((userChoice < 8) || (userChoice > 128)) {
       userChoice = parseInt(window.prompt("Enter the number of characters between 8 and 128: "));
-    
+  
       // Checking here to make sure the user entered a number and not a letter.
       if (isNaN(userChoice)) {
-        // This will reset the choice value so it can restart the loop if it's not a number
+        // This will restart the loop if user does not choose a number
         userChoice = 0;
       }
     }
-    
+  
     return userChoice;
-    }
-
-
-    // This function simplifies user  options by utilizing validation loops.  
-function getOption(currentOption) {
-    var userOption = "a",
+  }
+  
+  // This is a simplified function to be used with the validation loop "While".
+  function getChoice(promptedOption) {
+    var userChoice = "a",
       messagePrompt = "";
-    var messagePrompt = ('Would you like '.concat(currentOption));
+    var messagePrompt = ('Would you like '.concat(promptedOption));
     messagePrompt = messagePrompt.concat(' characters (y/n)?');
-    // This loop ensures the user enters a valid response.
-    while (userOption = "a") {
-      userOption = (window.prompt(messagePrompt));
-      // Added the line below for ease of usability on mobile devices as some of them automatically capitalize when entering input.
-      userOption = userOption.toLowerCase();
-      if (userOption == "y") {
+    // This loop makes sure the user enters a valid response.
+    while (userChoice = "a") {
+      userChoice = (window.prompt(messagePrompt));
+      // The line below will serve for responsiveness for mobile view. 
+      userChoice = userChoice.toLowerCase();
+      if (userChoice == "y") {
         return true;
-      } else if (userOption == "n") {
+      } else if (userChoice == "n") {
         return false;
       }
     }
-    }
-
-    var generateBtn = document.querySelector("#generate");
-
-    function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
-      
-        passwordText.value = password;
-      
-      }
-
-      generateBtn.addEventListener("click", writePassword);
+  }
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
